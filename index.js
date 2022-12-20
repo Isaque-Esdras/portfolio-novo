3a6d2be25bfc643eecc2307707db0a1fb23b7ab5
Isaque-Esdras <118394167+Explore-Apps@users.noreply.github.com>
2022-12-19 23:13:18 -0300

    a new

---
+++ index.js
@@ -0,0 +1,19 @@
+linkedin = () =>{
+    window.location.href = 'https://www.linkedin.com/in/isaque-costa-574890197/'
+}
+
+whatsapp = () =>{
+    window.location.href = 'http://api.whatsapp.com/send?1=pt_BR&phone=5575991593839'
+}
+
+sobre = ()=>{
+    window.location.href = '#contato'
+}
+
+contato = () =>{
+    window.location.href = '#contato'
+}
+
+cepbusque = () =>{
+    window.location.href = './cep-busque.html'
+}
