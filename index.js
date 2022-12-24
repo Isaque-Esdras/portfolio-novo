c73276a208cd5a051fe3d7b05b25f39356ac328d
Isaque-Esdras <118394167+Explore-Apps@users.noreply.github.com>
2022-12-24 01:35:17 -0300

    novo

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ contato = () =>{
 cepbusque = () =>{
     window.location.href = './cep-busque.html'
 }
+
+nubank = () =>{
+    window.location.href = './nubank.html'
+}
+
+cvc = () =>{
+    window.location.href = './cvc.html'
+}
+
+oculossite = () =>{
+    window.location.href = './oculos.html'
+}
+
+wpbarbeariavini = () =>{
+    window.location.href = './wpbarbeariaum.html'
+}
